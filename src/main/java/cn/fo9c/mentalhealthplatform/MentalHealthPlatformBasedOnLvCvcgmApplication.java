1e9d5001426d10cfda9400f80693a03f125edb79
fo9c <1974555620@qq.com>
2024-03-29 09:36:58 +0800

    Initial commit

---
+++ src/main/java/cn/fo9c/mentalhealthplatform/MentalHealthPlatformBasedOnLvCvcgmApplication.java
@@ -0,0 +1,13 @@
+package cn.fo9c.mentalhealthplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthPlatformBasedOnLvCvcgmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalHealthPlatformBasedOnLvCvcgmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fo9c/mentalhealthplatform/MentalHealthPlatformBasedOnLvCvcgmApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fo9c.mentalhealthplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthPlatformBasedOnLvCvcgmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
