2db230c458f8c017324e9048a014ac000d521e98
fo9c <1974555620@qq.com>
2024-05-14 21:55:22 +0800

    基本框架完善

--- src/main/java/cn/fo9c/mentalhealthplatform/MentalHealthPlatformBasedOnLvCvcgmApplication.java
+++ src/main/java/cn/fo9c/mentalhealthplatform/MentalHealthPlatformBasedOnLvCvcgmApplication.java
@@ -1,7 +1,9 @@
 package cn.fo9c.mentalhealthplatform;
 
+import com.baomidou.mybatisplus.core.MybatisPlusVersion;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
@@ -9,19 +11,30 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 @MapperScan("mapper")
 public class MentalHealthPlatformBasedOnLvCvcgmApplication {
-
+    // 新启动项追加路径显示
     public static void main(String[] args) {
 
-        // 新启动项追加路径显示
+        // 下面获取的是URL的环境变量
         ConfigurableApplicationContext application = SpringApplication.run(MentalHealthPlatformBasedOnLvCvcgmApplication.class, args);
         Environment env = application.getEnvironment();
         String port = env.getProperty("server.port");
         String property = env.getProperty("server.servlet.context-path");
         String path = property == null ? "" :  property;
+
+        // 下面获取的是SpringBoot的环境变量
+        String springBootVersion = SpringBootVersion.getVersion();
+
+        // 下面获取的是Mybatis-Plus的环境变量
+        Package pkg = MybatisPlusVersion.class.getPackage();
+        String mybatisPlusVersion = pkg.getImplementationVersion();
+
+
+
         System.out.println(
                 "———————————————————————————————————————————————————————————————————————————————————————————————————————————\n\t" +
-                        "         Access URLs:\thttp://localhost:" + port + path + "              SpringBoot   Version: 3.2.4 \n\t" +
-                        "         DataBase: MySQL                                   Mybatis-Plus Version: 3.5.5\n" +
+                        "         Access URLs:\thttp://localhost:" + port + path + "              SpringBoot   Version: " + springBootVersion + "\n\t" +
+                        "         DataBase:      MySQL                              Mybatis-Plus Version: " + mybatisPlusVersion + "\n\t" +
                         "———————————————————————————————————————————————————————————————————————————————————————————————————————————");
     }
+
 }
